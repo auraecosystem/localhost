3bbe89aef49fdf3d77daf2d278046f6e9d6fd5cc
Seriki Walter Yakub <webapp4@outlook.com>
2026-08-04 15:33:21 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,12 @@ LocalSettings.php:
 $wgDnsBlacklistUrls = array(
         'all.s5h.net.',
 );
+#!/bin/bash
+
+LOG=/var/tmp/my_script_${HOSTNAME}_$$_${USER}.log
+
+exec 2>&1 1>${LOG}
+$ my_script 2>&1 > /var/tmp/my_script_${HOSTNAME}_$$_${USER}.log
+You
+$ [alt-10]a
+$ aaaaaaaaaa
