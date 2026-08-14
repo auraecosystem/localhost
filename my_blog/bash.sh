6f0f28c15fe178cd7e4b9c52ef91d7a524591a85
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-15 00:39:34 +0100

    Create bash.sh

---
+++ my_blog/bash.sh
@@ -0,0 +1,8 @@
+# Option A: Astro (Recommended for content-focused blogs)
+npm create astro@latest my-blog -- --template blog
+
+# Option B: Next.js (Best if you need custom API routes or interactive React components)
+npx create-next-app@latest my-blog
+
+# Option C: Hugo (Fast Go-based SSG)
+hugo new site my-blog
