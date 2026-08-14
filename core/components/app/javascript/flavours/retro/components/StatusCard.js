f7956e84f2268f94e53a2887e6dcde9d95b32470
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-15 00:28:41 +0100

    Create StatusCard.js

---
+++ core/components/app/javascript/flavours/retro/components/StatusCard.js
@@ -0,0 +1,3 @@
+// Inside a flavour component: app/javascript/flavours/retro/components/StatusCard.js
+import { useUserData } from 'core/hooks/useUserData'; // Shared logic
+import 'flavours/retro/styles/status-card.scss';     // Flavour styling
