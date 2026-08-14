364c1a7b147a14e002bffd712dc5e1c3e712c479
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-14 19:14:44 +0100

    Update update_readme.py

--- .github/actions-runner/update_readme.py
+++ .github/actions-runner/update_readme.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 # Configuration
-FEED_URL = "https://qubuhub.blogspot.com"  # Replace with your actual feed URL
+FEED_URL = "https://page.net"  # Replace with your actual feed URL
 README_PATH = "profile/README.md"
 MAX_POSTS = 10
 
